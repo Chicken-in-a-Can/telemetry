76da74dccc1ae4d72fba398db9ba76d0e9218ab6
Chicken-in-a-Can <ben.stidham1@gmail.com>
2022-09-01 10:54:13 -0500

    I trieded

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # Import modules
 
 # Tkinter for GUI window
@@ -7,11 +8,17 @@ from tkinter import ttk
 import os
 
 # System Vars
-usbs = os.popen("lsusb").read()
+usbs = os.popen("cat /etc/os-release").read()
+pings = os.popen("lsusb").read()
 
 
 # Methods
+def close():
+    root.quit()
 
+def update():
+    thecmd = os.popen("lsusb").read()
+    lsusb.config(text = thecmd)
 
 # Main code start
 
@@ -19,8 +26,6 @@ usbs = os.popen("lsusb").read()
 root = tk.Tk()
 root.title("Solar Car Telemetry")
 root.attributes("-fullscreen", True)
-
-
 
 # Tab initialization
 tabControl = ttk.Notebook(root)
@@ -34,7 +39,27 @@ tabtitle = ["Home", "Main Power", "Auxillary Power"]
 for i in range(len(tabs)):
     tabControl.add(tabs[i], text = tabtitle[i])
 
+lsusb = tk.Label(homeTab, text=pings, font = ("Times", 10))
+lsusb.grid(column = 0, row = 5, padx = 10, pady = 10)
+
+the_button = tk.Button(homeTab, text="update", command = update)
+the_button.grid(column = 1, row = 10, padx = 10, pady = 10)
+
+exit_button = tk.Button(homeTab, text="Exit", font=("Times", 10), command=close)
+exit_button.grid(column = 10, row = 10, padx = 10, pady = 10)
 
 # End Assembly
 tabControl.grid(column = 1, row = 0, padx = 10, pady = 10)
+
+def updater():
+    try:
+        ping2 = os.popen("lsusb").read()
+        if pings != ping2:
+            update()
+            pings = os.popen("lsusb")
+        ping2 = os.popen("lsusb").read()
+    except NameError:
+        pings = os.popen("lsusb").read()
+
+root.after(1000, updater())
 root.mainloop()
