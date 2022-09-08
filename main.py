5edee42a9695f040795241527998f3aa5ab58f91
Chicken-in-a-Can <ben.stidham1@gmail.com>
2022-09-08 09:57:39 -0500

    Added multi-tab updates

--- main.py
+++ main.py
@@ -19,14 +19,32 @@ ram = os.popen("free -h").read()
 def close():
     root.quit()
 
-def update():
+def update_home():
     thecmd = os.popen("free -h").read()
-    freeram.config(text = thecmd)
+    home_info_one.config(text = thecmd)
+
+def update_main_power():
+    thecmd = os.popen("date +'TIME: %T'").read()
+    main_power_info_one.config(text = thecmd)
+
+def update_aux_power():
+    thecmd = os.popen("upower -i $(upower -e | grep BAT)").read()
+    aux_power_info_one.config(text = thecmd)
 
 def continueupdate():
     while True:
-        update()
+        tabupdatechanger()
         time.sleep(0.1)
+
+def tabupdatechanger():
+    tab_index = tabControl.index(tabControl.select())
+    match tab_index:
+        case 0:
+            update_home()
+        case 1:
+            update_main_power()
+        case 2:
+            update_aux_power()
 
 # Main code start
 
@@ -47,11 +65,26 @@ tabtitle = ["Home", "Main Power", "Auxillary Power"]
 for i in range(len(tabs)):
     tabControl.add(tabs[i], text = tabtitle[i])
 
-freeram = tk.Label(homeTab, text=ram, font = ("Times", 10))
-freeram.grid(column = 0, row = 5, padx = 10, pady = 10)
+# Home Tab
+home_info_one = tk.Label(homeTab, text=ram, font = ("Times", 10))
+home_info_one.grid(column = 0, row = 5, padx = 10, pady = 10)
 
-exit_button = tk.Button(homeTab, text="Exit", font=("Times", 10), command=close)
+exit_button = tk.Button(homeTab, text="Exit", font=("Times", 10), command=close, anchor = "w")
 exit_button.grid(column = 10, row = 10, padx = 10, pady = 10)
+
+
+
+# Main Power Tab
+main_power_info_one = tk.Label(powerTab, text=ram, font = ("Times", 10))
+main_power_info_one.grid(column = 0, row = 5, padx = 10, pady = 10)
+
+
+
+# Auxillary Power Tab
+aux_power_info_one = tk.Label(auxTab, text=ram, font = ("Times", 10))
+aux_power_info_one.grid(column = 0, row = 5, padx = 10, pady = 10)
+
+
 
 # End Assembly
 tabControl.grid(column = 1, row = 0, padx = 10, pady = 10)
